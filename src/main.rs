be4fc606ec4b4a968a0f98af5330b647f9b5ff04
danmandel <dan19926@gmail.com>
2024-05-12 01:21:41 -0400

    alpaca client setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Starting the automated trading system.");
+use dotenv::dotenv;
+use reqwest::{header::HeaderMap, Client};
+use serde::Deserialize;
+use std::env;
+use automated_trading_system::datastructures::config::Config;
+
+// use alpaca::{AlpacaClient};
+mod alpaca;
+use alpaca::AlpacaClient;
+
+#[derive(Debug, Deserialize)]
+struct Asset {
+    symbol: String,
+    exchange: String,
 }
+
+#[tokio::main]
+async fn main() {
+    let config = Config::builder()
+        .alpaca_api_key(env::var("ALPACA_API_KEY").expect("API key must be set"))
+        .alpaca_secret_key(env::var("ALPACA_SECRET_KEY").expect("Secret key must be set"))
+        .alpaca_base_url(env::var("ALPACA_BASE_URL").expect("Base URL must be set"))
+        .build();
+
+    let client = AlpacaClient::new(&config); 
+
+    println!("🚀 The automated trading system is live. 📈");
+}
+
