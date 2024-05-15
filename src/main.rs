8cfaaa0091710686e7a49715daed7129ba488185
danmandel <dan19926@gmail.com>
2024-05-15 00:21:21 -0400

    make trading_client its own crate

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,20 @@
 use dotenv::dotenv;
-use reqwest::{header::HeaderMap, Client};
-use serde::Deserialize;
 use std::env;
-use automated_trading_system::datastructures::config::Config;
-
-// use alpaca::{AlpacaClient};
-mod alpaca;
-use alpaca::AlpacaClient;
-
-#[derive(Debug, Deserialize)]
-struct Asset {
-    symbol: String,
-    exchange: String,
-}
+use trading_client::alpaca::AlpacaClient;
+use trading_client::datastructures::client::TradingClient;
+use trading_client::datastructures::config::Config;
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
+
     let config = Config::builder()
         .alpaca_api_key(env::var("ALPACA_API_KEY").expect("API key must be set"))
         .alpaca_secret_key(env::var("ALPACA_SECRET_KEY").expect("Secret key must be set"))
         .alpaca_base_url(env::var("ALPACA_BASE_URL").expect("Base URL must be set"))
         .build();
 
-    let client = AlpacaClient::new(&config); 
+    let client = AlpacaClient::new(&config);
 
     println!("🚀 The automated trading system is live. 📈");
 }
-
