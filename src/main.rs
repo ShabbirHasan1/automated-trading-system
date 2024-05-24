5ffaf4f8a858e4d14a1a2ed12fa7f3f5d17c5d4f
danmandel <dan19926@gmail.com>
2024-05-24 17:27:08 -0400

    use SubscriptionRequest builder

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,17 @@ use automated_trading_system::strategies::dip_buy_spy_calls::DipBuySpyCallsStrat
 use automated_trading_system::strategies::moving_average_crossover::MovingAverageCrossOverStrategy;
 use automated_trading_system::strategy::Strategy;
 use dotenv::dotenv;
+use futures_util::StreamExt;
 use std::env;
+use tokio_tungstenite::tungstenite::protocol::Message;
+use tokio_tungstenite::{MaybeTlsStream, WebSocketStream};
 use trading_client::alpaca::AlpacaClient;
 use trading_client::datastructures::client::TradingClient;
+use trading_client::datastructures::client::{FeedType, SubscriptionRequestBuilder};
 use trading_client::datastructures::config::Config; // TODO: use lib to shorten import path. or use automated_trading_system::strategy::Strategy;
-use trading_client::datastructures::client::FeedType;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("System starting.");
 
     dotenv().ok();
@@ -25,35 +28,39 @@ async fn main() {
 
     println!("🚀 The automated trading system is live. 📈");
 
-    let x = client.get_asset("AAPL").await;
-    println!("Asset data for AAPL: {:?}", x);
-
-
     // Each `strategy` has its own instance of `client` that will track history. TODO: push to database or redis for single source of truth.
     let mut strategies: Vec<Box<dyn Strategy>> = vec![
         Box::new(DipBuySpyCallsStrategy::new(client.clone())),
         Box::new(MovingAverageCrossOverStrategy::new(client.clone())),
     ];
 
-    let symbol = "BTC/USD";
-    // let symbols = ["SPY", "BTC"];
+    let request = SubscriptionRequestBuilder::new()
+        .trades(&["AAPL", "GOOGL"])
+        .quotes(&["AAPL", "GOOGL"])
+        .bars(&["AAPL", "GOOGL"])
+        .updated_bars(&["AAPL", "GOOGL"])
+        .daily_bars(&["AAPL", "GOOGL"])
+        .orderbooks(&["AAPL", "GOOGL"])
+        .build();
 
-    // // TODO: Loop over symbols. Should strategies have their own symbol set?
-    // // What if 1 strategy = 1 async process running independently.
+    let ws_stream = client.subscribe(request, FeedType::Stocks).await?;
+    let (_, mut incoming_messages) = ws_stream.split();
 
-    if let Ok(socket) = client.subscribe_to_data(symbol, FeedType::Stocks).await {
-        println!("Success")
-        // while let Some(message) = socket.next().await {
-        //     if let Ok(Message::Text(text)) = message {
-        //         // Clone strategies for each tick handling
-        //         let strategies_clone = strategies.clone();
-        //         handle_tick_concurrently(text, strategies_clone).await;
-        //     }
-        // }
-        // run_trading_client(socket, strategies).await;
-    } else {
-        eprintln!("Failed to connect to data stream.");
+    while let Some(msg) = incoming_messages.next().await {
+        match msg {
+            Ok(message) => match message {
+                Message::Text(text) => println!("Received: {}", text),
+                Message::Binary(bin) => println!("Received binary data: {:?}", bin),
+                _ => (),
+            },
+            Err(err) => {
+                // Handle any errors that occur during the subscription
+                eprintln!("Error: {:?}", err);
+                // You can choose to break the loop or continue based on your error handling strategy
+            }
+        }
     }
 
     println!("System shutdown.");
+    Ok(())
 }
